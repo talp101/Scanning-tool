acdd864f946fa86c85c8a13c3a54b125fc8af000
Tal Peretz <13@1500.co.il>
2014-03-29 23:22:22 -0700

    Creating Basics Func and structre

--- Menu/menu.py
+++ Menu/menu.py
@@ -6,8 +6,11 @@ from PortScanning import scanner
 
 
 def main():
-    s = scanner.Scanner("82.166.60.130", "127.0.0.1")
+    s = scanner.Scanner("212.71.233.149", "127.0.0.1")
     print "created s"
+    print s.dst_ip
+    print s.src_port
+    print s.src_ip
     s.tcp_scan()
     print "End"
 
--- PortScanning/scanner.py
+++ PortScanning/scanner.py
@@ -13,14 +13,14 @@ class Scanner():
         self.results = {1: "Open", 2: "Closed", 3: "Filtered"}
 
     def tcp_scan(self):
-        p = IP(dst=self.dst_ip, src=self.src_ip) / TCP(sport=self.src_port, dport=self.dst_ports[0], flags="S")
-        resp = sr1(p, timeout=10)
+        # p = IP(dst=self.dst_ip, src=self.src_ip) / TCP(sport=self.src_port, dport=80, flags="S")
+        resp = sr1(IP(dst=self.dst_ip, src=self.src_ip) / TCP(sport=self.src_port, dport=80, flags="S"), timeout=10)
         if str(type(resp)) == "<type 'NoneType'>":
             print self.results[2]
         elif resp.haslayer(TCP):
             if resp.getlayer(TCP).flags == 0x12:
                 send_rst = sr(
-                    IP(dst=self.dst_ip, src=self.src_ip) / TCP(sport=self.src_port, dport=self.dst_ports[0], flags="AR"),
+                    IP(dst=self.dst_ip, src=self.src_ip) / TCP(sport=self.src_port, dport=80, flags="AR"),
                     timeout=10)
                 print self.results[1]
             elif resp.getlayer(TCP).flags == 0x14:
